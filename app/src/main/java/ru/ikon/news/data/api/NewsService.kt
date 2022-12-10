158356928770434970a481893eb8f48464cffa3a
Ik0n <Ik0nChannel1@mail.ru>
2022-12-10 11:21:56 +0300

    Lesson03

---
+++ app/src/main/java/ru/ikon/news/data/api/NewsService.kt
@@ -0,0 +1,21 @@
+package ru.ikon.news.data.api
+
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.ikon.news.utils.Constants.Companion.API_KEY
+
+interface NewsService {
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+
+    suspend fun getHeadLines(
+        @Query("country") countryCode: String = "ru",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+}
